dfa26ca690d5be6b813d1983a810749652a7aa06
Andreas <a.oshtman@gmail.com>
2018-01-27 11:14:20 +0100

    - Added some bindings to ASDisplayNode

---
+++ ASDisplayNode+Rx.swift
@@ -0,0 +1,8 @@
+//
+//  ASDisplayNode+Rx.swift
+//  Alamofire
+//
+//  Created by Andreas Östman on 2018-01-27.
+//
+
+import Foundation
---
+++ RxTexture/Classes/ASDisplayNode+Rx.swift
@@ -0,0 +1,24 @@
+//
+//  ASDisplayNode+Rx.swift
+//  Alamofire
+//
+//  Created by Andreas Östman on 2018-01-27.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+import AsyncDisplayKit
+
+extension Reactive where Base: ASDisplayNode {
+    public var visible: Binder<BooL> {
+        return Binder
+    }
+//
+//    public var visible: Binder<Bool> {
+//        return Binder(self.base) { node, value in
+//
+//            node.isVisible = value
+//        }
+//    }
+}
